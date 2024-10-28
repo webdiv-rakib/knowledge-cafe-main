9ccf8d3534f452e8351fd2cd23db65ac8e067511
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-10-28 18:00:27 +0600

    Dynamically display blog and hashtags

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,9 +1,28 @@
 import PropTypes from 'prop-types';
 const Blog = ({ blog }) => {
     console.log(blog);
+    const { title, cover, reading_time, author_img, author, posted_date, hashtags } = blog;
     return (
         <div>
-
+            <img src={cover} alt="" />
+            <div className='flex justify-between items-center'>
+                <div className='flex items-center gap-2'>
+                    <img className='h-[60px]' src={author_img} alt="" />
+                    <div>
+                        <h1>{author}</h1>
+                        <h3>{posted_date}</h3>
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time} min read</span>
+                </div>
+            </div>
+            <h2 className='text-4xl'>{title}</h2>
+            <p>
+                {
+                    hashtags.map((hash, idx) => <span key={idx}><a href="">#{hashtags}</a></span>)
+                }
+            </p>
         </div>
     );
 };
--- src/components/Header/Blogs/Blogs.jsx
+++ src/components/Header/Blogs/Blogs.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Blog from "../../Blog/Blog";
 
 const Blogs = () => {
     const [blogs, setBlogs] = useState([]);
@@ -7,10 +8,17 @@ const Blogs = () => {
         fetch('blog.json')
             .then(res => res.json())
             .then(data => setBlogs(data))
+            .catch(error => console.log(error))
     }, [])
     return (
         <div className="md:w-2/3">
             <h1 className="text-4xl">Blogs: {blogs.length}</h1>
+            {
+                blogs.map(blog => <Blog
+                    key={blog.id}
+                    blog={blog}
+                ></Blog>)
+            }
         </div>
     );
 };
